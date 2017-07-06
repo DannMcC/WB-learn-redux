ddad72bd8ad1a86e8a41018daf6724aaceb25d39
Dan McCune <dmccune@hivelocity.net>
2017-07-06 10:33:25 -0400

    hello router

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -2,9 +2,22 @@ import React from 'react';
 
 import { render } from 'react-dom';
 
-//impo
+//import
 import css from './styles/style.styl';
 
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
-render(<Main/>, document.getElementById('root'));
+import { Router,  Route, IndexRoute, BrowserHistory } from 'react-router';
+
+const router = (
+    <Router history={BrowserHistory}>
+        <Route path='/' component={Main}>
+            <IndexRoute component={PhotoGrid}></IndexRoute>
+            <Route path='/view/:postId' component={Single}></Route>
+        </Route>
+    </Router>
+)
+
+render(router, document.getElementById('root'));
