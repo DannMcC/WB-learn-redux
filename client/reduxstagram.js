9903555844050d1d9fb0adf956651482e37f8b34
Dan McCune <dmccune@hivelocity.net>
2017-07-07 14:38:17 -0400

    Hello Store

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -9,10 +9,10 @@ import Main from './components/Main';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
-import { Router,  Route, IndexRoute, BrowserHistory } from 'react-router';
+import { Router,  Route, IndexRoute, browserHistory } from 'react-router';
 
 const router = (
-    <Router history={BrowserHistory}>
+    <Router history={browserHistory}>
         <Route path='/' component={Main}>
             <IndexRoute component={PhotoGrid}></IndexRoute>
             <Route path='/view/:postId' component={Single}></Route>
